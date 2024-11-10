7f4c23951ab079aca2323dee3144c4680e7a0b2d
Denar Ahmaron <denarahmaron1@gmail.com>
2024-11-10 14:00:03 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const TodolistService = require("./service-todolist");
+
+const service = new TodolistService();
+const server = http.createServer((request, response) => {
+  response.setHeader("Content-Type", "application/json");
+
+  if (request.method === "GET") {
+    service.getTodoList(request, response);
+  } else if (request.method === "POST") {
+    service.createTodoList(request, response);
+  } else if (request.method === "PUT") {
+    service.updateTodoList(request, response);
+  } else if (request.method === "DELETE") {
+    service.deleteTodoList(request, response);
+  }
+});
+
+server.listen(3000);
